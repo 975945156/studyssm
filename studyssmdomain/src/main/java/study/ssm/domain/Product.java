0708e9d6a5608775ea8f8099904558c87907eca8
xuzhiqiang <975945156@qq.com>
2020-10-23 18:04:25 +0800

    更新ssm-domain下的Product.java

--- studyssmdomain/src/main/java/study/ssm/domain/Product.java
+++ studyssmdomain/src/main/java/study/ssm/domain/Product.java
@@ -1,5 +1,7 @@
 package study.ssm.domain;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 /*
 *编写实体类
@@ -10,6 +12,7 @@ public class Product {
     private String productNum; // 编号 唯一
     private String productName; // 名称
     private String cityName; // 出发城市
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
     private Date departureTime; // 出发时间
     private String departureTimeStr;
     private double productPrice; // 产品价格
@@ -58,6 +61,9 @@ public class Product {
     }
 
     public String getDepartureTimeStr() {
+        if(departureTime!=null){
+            departureTimeStr= DateUtils.date2String(departureTime,"yyyy-MM-dd HH:mm:ss");
+        }
         return departureTimeStr;
     }
 
@@ -90,6 +96,13 @@ public class Product {
     }
 
     public String getProductStatusStr() {
+        if (productStatus != null) {
+            // 状态 0 关闭 1 开启
+            if(productStatus==0)
+                productStatusStr="关闭";
+            if(productStatus==1)
+                productStatusStr="开启";
+        }
         return productStatusStr;
     }
 
@@ -97,3 +110,4 @@ public class Product {
         this.productStatusStr = productStatusStr;
     }
 }
+
